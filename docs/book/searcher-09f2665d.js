4a0245ad99c502cd6ebf86644d09b041afdf238c
Eric Stokes <eestokes@pm.me>
2026-09-11 12:02:12 -0400

    rebuild book

--- docs/book/searcher-09f2665d.js
+++ docs/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-682fa164.js',
+                path_to_root + 'searchindex-ba386dbe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
